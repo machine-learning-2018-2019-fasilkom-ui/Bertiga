bb0d29936058770bfa428a698efea100d37a18eb
dipoa <dipoanugrah96@gmail.com>
2019-05-09 21:52:00 +0700

    adjust dataset

--- data/readjson.py
+++ data/readjson.py
@@ -1,7 +1,8 @@
 import json_lines
+import codecs
 
-negFile = open('rt-polarity.neg', 'a')
-posFile = open('rt-polarity.pos', 'a')
+negFile = codecs.open('rt-polarity.neg', 'w', 'utf-8')
+posFile = codecs.open('rt-polarity.pos', 'w', 'utf-8')
 gameReview = ['Arma_3', 'Counter_Strike', 'Counter_Strike_Global_Offensive', 'Dota_2', 'Football_Manager_2015',
               'Garrys_Mod', 'Grand_Theft_Auto_V', 'Sid_Meiers_Civilization_5', 'Team_Fortress_2', 'The_Elder_Scrolls_V', 'Warframe']
 for game in gameReview:
